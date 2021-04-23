ccf44423841994990bb5f3708064a730a9eeeea0
ciscoff <ciscoff@ya.ru>
2021-04-23 18:12:42 +0300

    deep link

---
+++ app/src/main/java/s/yarlykov/izinavigation/ui/DeepActivity.kt
@@ -0,0 +1,12 @@
+package s.yarlykov.izinavigation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import s.yarlykov.izinavigation.R
+
+class DeepActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deep)
+    }
+}
