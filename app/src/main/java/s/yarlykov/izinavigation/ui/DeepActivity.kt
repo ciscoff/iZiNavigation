5084daaa29f1968d2675edb1532428e15686562f
ciscoff <ciscoff@ya.ru>
2021-04-26 19:26:22 +0300

    deep link

--- app/src/main/java/s/yarlykov/izinavigation/ui/DeepActivity.kt
+++ app/src/main/java/s/yarlykov/izinavigation/ui/DeepActivity.kt
@@ -2,11 +2,47 @@ package s.yarlykov.izinavigation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import s.yarlykov.izinavigation.R
+
+/**
+ * Тестирование через adb:
+ *
+ * cd /d C:\Users\syarlykov\AppData\Local\Android\Sdk\platform-tools
+ * adb shell am start -W -a android.intent.action.VIEW -d "sy://gizmos" s.yarlykov.izinavigation
+ *
+ *  или так, без пакета
+ *
+ * adb shell am start -W -a android.intent.action.VIEW -d "sy://gizmos"
+ */
 
 class DeepActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deep)
+    }
+
+    @ExperimentalCoroutinesApi
+    private fun tempFun() {
+
+        val flow = flow {
+            coroutineScope {
+                emit(10)
+            }
+        }
+
+        val flow2 = channelFlow {
+            send(1)
+        }
+
+
+        lifecycleScope.launch {
+
+        }
     }
 }
---
+++ app/src/main/java/s/yarlykov/izinavigation/utils/Notifier.kt
@@ -0,0 +1,59 @@
+package s.yarlykov.izinavigation.utils
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import s.yarlykov.izinavigation.R
+
+/**
+ * Utility class for posting notifications.
+ * This class creates the notification channel (as necessary) and posts to it when requested.
+ */
+class Notifier(context: Context) {
+
+    companion object {
+        private const val channelId = "Default"
+    }
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager =
+                context.getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+
+            val existingChannel = notificationManager.getNotificationChannel(channelId)
+
+            if (existingChannel == null) {
+                val name = context.getString(R.string.default_channel)
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val mChannel = NotificationChannel(channelId, name, importance)
+                mChannel.description = context.getString(R.string.notification_description)
+                notificationManager.createNotificationChannel(mChannel)
+            }
+        }
+
+    }
+
+    fun postNotification(id: Long, context: Context, intent: PendingIntent) {
+        val builder = NotificationCompat.Builder(context, channelId)
+        builder.setContentTitle(context.getString(R.string.deepLink_notification_title))
+
+            .setSmallIcon(R.drawable.ic_dashboard)
+        val text = context.getString(R.string.show_more_dashboards)
+
+        val notification = builder.setContentText(text)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(intent)
+            .setAutoCancel(true)
+            .build()
+        val notificationManager = NotificationManagerCompat.from(context)
+
+        // Remove prior notifications; only allow one at a time to edit the latest item
+        notificationManager.cancelAll()
+        notificationManager.notify(id.toInt(), notification)
+    }
+}
